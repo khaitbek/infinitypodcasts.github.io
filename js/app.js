aedcf1564414b0065f454872c577c651045b9542
Khaitbek <@khaitbek2005@gmail.com>
2021-08-30 11:32:18 +0500

    Finished

--- js/app.js
+++ js/app.js
@@ -2,28 +2,28 @@ let audio = document.querySelectorAll(".audio");
 const section = document.querySelector(".podcasts");
 let btns = document.querySelectorAll(".categories button");
 let btn_container = document.querySelector(".categories");
-console.log(btn_container.children);
+
 let podcasts = [
   {
     id: 1,
     audio: "./podcasts/Interview/STEVEJOBSANDBILLGATES.mp3",
     title: "Steve Jobs and Bill Gates",
     category: "INTERVIEWS",
-    img: "./images/stevejobsandbillgates.jpg",
+    img: "./images/podcast.jpg",
   },
   {
     id: 2,
     audio: "./podcasts/Interview/IASKEDBILLGATESWHAT'STHENEXTCRISIS..mp3",
     title: "Bill Gates talking about the next crisis",
     category: "INTERVIEWS",
-    img: "./images/bill_gates.jpg",
+    img: "./images/podcast.jpg",
   },
   {
     id: 3,
     audio: "./podcasts/Interview/STEVEJOBS10MINUTESOFAMAZINGBUSINESSADVICE.m4a",
     title: "10 minutes of Amazing Business Advice from Steve Jobs",
     category: "INTERVIEWS",
-    img: "./images/stevejobs1.jpg",
+    img: "./images/podcast.jpg",
   },
 
   {
@@ -31,93 +31,206 @@ let podcasts = [
     audio: "./podcasts/IELTS/IELTS SPEAKING - HOW TO INTRODUCE YOURSELF.mp3",
     title: "IELTS SPEAKING - How To Introduce Yourself",
     category: "IELTS",
-    img: "./images/ieltsspeaking1.jpg",
+    img: "./images/podcast.jpg",
   },
   {
     id: 5,
     audio: "./podcasts/IELTS/IELTS SPEAKING BAND 9 CONVERSATION & TIPS.mp3",
     title: "IELTS SPEAKING BAND 9 TIPS",
     category: "IELTS",
-    img: "./images/ieltsband9.png",
+    img: "./images/podcast.jpg",
   },
   {
     id: 6,
     audio: "./podcasts/IELTS/IELTS SPEAKING TEST BAND SCORE 9.0.mp3",
     title: "IELTS SPEAKING TEST BAND 9",
     category: "IELTS",
-    img: "./images/ielts1.jpeg",
+    img: "./images/podcast.jpg",
   },
   {
     id: 7,
     audio: "./podcasts/IELTS/USEFUL IDIOMS FOR ANY TOPIC IN IELTS SPEAKING.mp3",
     title: "Useful idioms for any topic in IELTS speaking",
     category: "IELTS",
-    img: "./images/ieltsidioms.png",
+    img: "./images/podcast.jpg",
   },
   {
     id: 8,
     audio: "./podcasts/Motivation/DEDICATION.mp3",
     title: "Dedication",
     category: "MOTIVATION",
-    img: "./images/dedication1.png",
+    img: "./images/podcast.jpg",
   },
   {
     id: 9,
     audio: "./podcasts/Motivation/RESET YOUR MINDSET.mp3",
     title: "Reset your mindset",
     category: "MOTIVATION",
-    img: "./images/newmindset.jpg",
+    img: "./images/podcast.jpg",
   },
   {
     id: 10,
     audio: "./podcasts/Motivation/WHAT DO ALL GREAT LEADERS HAVE IN COMMON.mp3",
     title: "What do all leaders have in common",
     category: "MOTIVATION",
-    img: "./images/leaders.jpg",
+    img: "./images/podcast.jpg",
   },
   {
     id: 11,
     audio: "./podcasts/Motivation/YOU VS YOU.mp3",
     title: "You vs You",
     category: "MOTIVATION",
-    img: "./images/youvsyou.png",
+    img: "./images/podcast.jpg",
   },
   {
     id: 12,
     audio: "./podcasts/Interview/BILLIONAIRES JACK MA VS ELON MUSK DEBATE.mp3",
     title: "Jack Ma vs Elon Musk",
     category: "INTERVIEWS",
-    img: "./images/jackmaelonmusk.jpg",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 13,
+    audio: "./podcasts/Interview/BILLIONAIRES JACK MA VS ELON MUSK DEBATE.mp3",
+    title: "Jack Ma vs Elon Musk",
+    category: "BUSINESS",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 14,
+    audio: "./podcasts/Interview/STEVEJOBSANDBILLGATES.mp3",
+    title: "Steve Jobs and Bill Gates",
+    category: "BUSINESS",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 15,
+    audio: "./podcasts/Business/HOW TESLA BUILDS CARS.mp3",
+    title: "How Tesla builds cars?",
+    category: "BUSINESS",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 16,
+    audio: "./podcasts/Business/APPLE VS TESLA.mp3",
+    title: "Apple vs Tesla",
+    category: "BUSINESS",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 17,
+    audio: "./podcasts/Music/EMINEM - TILL I COLLAPSE.mp3",
+    title: "Eminem - Till I Collapse",
+    category: "SONGS",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 18,
+    audio: "./podcasts/Music/JUSTIN BIEBER - LET ME LOVE YOU.m4a",
+    title: "Justin Bieber - Let me love you",
+    category: "SONGS",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 19,
+    audio: "./podcasts/Music/JUSTIN TIMBERLAKE - YOUNG MAN.mp3",
+    title: "Jack Ma vs Elon Musk",
+    category: "SONGS",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 20,
+    audio: "./podcasts/Music/WIZ KHALIF ft CHARLIE PUTH - SEE YOU AGAIN.mp3",
+    title: "Wiz Khalif ft Charlie Puth - See you Again",
+    category: "SONGS",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 21,
+    audio: "./podcasts/Stories/Clever Shoemaker.mp3",
+    title: "Clever Shoemaker",
+    category: "STORIES",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 22,
+    audio: "./podcasts/Stories/Mother Holle.mp3",
+    title: "Mother Holle",
+    category: "STORIES",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 23,
+    audio: "./podcasts/Stories/The Golden Bird.mp3",
+    title: "The Golden Bird",
+    category: "STORIES",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 24,
+    audio: "./podcasts/Stories/The Greedy Milkman.mp3",
+    title: "The greedy Milkman",
+    category: "STORIES",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 25,
+    audio: "./podcasts/Stories/Clever Shoemaker.mp3",
+    title: "Clever Shoemaker",
+    category: "ELEMENTARY",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 26,
+    audio: "./podcasts/Stories/Mother Holle.mp3",
+    title: "Mother Holle",
+    category: "ELEMENTARY",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 27,
+    audio: "./podcasts/Stories/The Golden Bird.mp3",
+    title: "The Golden Bird",
+    category: "ELEMENTARY",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 28,
+    audio: "./podcasts/Stories/The Greedy Milkman.mp3",
+    title: "The greedy Milkman",
+    category: "ELEMENTARY",
+    img: "./images/podcast.jpg",
   },
 ];
+
 let interviews = [
   {
     id: 1,
     audio: "./podcasts/Interview/STEVEJOBSANDBILLGATES.mp3",
     title: "Steve Jobs and Bill Gates",
     category: "INTERVIEWS",
-    img: "./images/stevejobsandbillgates.jpg",
+    img: "./images/podcast.jpg",
   },
   {
     id: 2,
     audio: "./podcasts/Interview/IASKEDBILLGATESWHAT'STHENEXTCRISIS..mp3",
     title: "Bill Gates talking about the next crisis",
     category: "INTERVIEWS",
-    img: "./images/bill_gates.jpg",
+    img: "./images/podcast.jpg",
   },
   {
     id: 3,
     audio: "./podcasts/Interview/STEVEJOBS10MINUTESOFAMAZINGBUSINESSADVICE.m4a",
     title: "10 minutes of Amazing Business Advice from Steve Jobs",
     category: "INTERVIEWS",
-    img: "./images/stevejobs1.jpg",
+    img: "./images/podcast.jpg",
   },
   {
     id: 4,
     audio: "./podcasts/Interview/BILLIONAIRES JACK MA VS ELON MUSK DEBATE.mp3",
     title: "Jack Ma vs Elon Musk",
     category: "INTERVIEWS",
-    img: "./images/jackmaelonmusk.jpg",
+    img: "./images/podcast.jpg",
   },
  
 ];
@@ -127,28 +240,28 @@ let IELTS = [
     audio: "./podcasts/IELTS/IELTS SPEAKING - HOW TO INTRODUCE YOURSELF.mp3",
     title: "IELTS SPEAKING - How To Introduce Yourself",
     category: "IELTS",
-    img: "./images/ieltsspeaking1.jpg",
+    img: "./images/podcast.jpg",
   },
   {
     id: 5,
     audio: "./podcasts/IELTS/IELTS SPEAKING BAND 9 CONVERSATION & TIPS.mp3",
     title: "IELTS SPEAKING BAND 9 TIPS",
     category: "IELTS",
-    img: "./images/ieltsband9.png",
+    img: "./images/podcast.jpg",
   },
   {
     id: 6,
     audio: "./podcasts/IELTS/IELTS SPEAKING TEST BAND SCORE 9.0.mp3",
     title: "IELTS SPEAKING TEST BAND 9",
     category: "IELTS",
-    img: "./images/ielts1.jpeg",
+    img: "./images/podcast.jpg",
   },
   {
     id: 7,
     audio: "./podcasts/IELTS/USEFUL IDIOMS FOR ANY TOPIC IN IELTS SPEAKING.mp3",
     title: "Useful idioms for any topic in IELTS speaking",
     category: "IELTS",
-    img: "./images/ieltsidioms.png",
+    img: "./images/podcast.jpg",
   },
 ];
 let motivation = [
@@ -157,32 +270,151 @@ let motivation = [
     audio: "./podcasts/Motivation/DEDICATION.mp3",
     title: "Dedication",
     category: "MOTIVATION",
-    img: "./images/dedication1.png",
+    img: "./images/podcast.jpg",
   },
   {
     id: 9,
     audio: "./podcasts/Motivation/RESET YOUR MINDSET.mp3",
     title: "Reset your mindset",
     category: "MOTIVATION",
-    img: "./images/newmindset.jpg",
+    img: "./images/podcast.jpg",
   },
   {
     id: 10,
     audio: "./podcasts/Motivation/WHAT DO ALL GREAT LEADERS HAVE IN COMMON.mp3",
     title: "What do all leaders have in common",
     category: "MOTIVATION",
-    img: "./images/leaders.jpg",
+    img: "./images/podcast.jpg",
   },
   {
     id: 11,
     audio: "./podcasts/Motivation/YOU VS YOU.mp3",
     title: "You vs You",
     category: "MOTIVATION",
-    img: "./images/youvsyou.png",
+    img: "./images/podcast.jpg",
   },
 
 ];
-
+let business = [
+  {
+    id: 1,
+    audio: "./podcasts/Interview/BILLIONAIRES JACK MA VS ELON MUSK DEBATE.mp3",
+    title: "Jack Ma vs Elon Musk",
+    category: "BUSINESS",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 2,
+    audio: "./podcasts/Interview/STEVEJOBSANDBILLGATES.mp3",
+    title: "Steve Jobs and Bill Gates",
+    category: "BUSINESS",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 3,
+    audio: "./podcasts/Business/HOW TESLA BUILDS CARS.mp3",
+    title: "How Tesla builds cars?",
+    category: "BUSINESS",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 4,
+    audio: "./podcasts/Business/APPLE VS TESLA.mp3",
+    title: "Apple vs Tesla",
+    category: "BUSINESS",
+    img: "./images/podcast.jpg",
+  },
+]
+let stories = [
+  {
+    id: 21,
+    audio: "./podcasts/Stories/Clever Shoemaker.mp3",
+    title: "Clever Shoemaker",
+    category: "STORIES",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 22,
+    audio: "./podcasts/Stories/Mother Holle.mp3",
+    title: "Mother Holle",
+    category: "STORIES",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 23,
+    audio: "./podcasts/Stories/The Golden Bird.mp3",
+    title: "The Golden Bird",
+    category: "STORIES",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 24,
+    audio: "./podcasts/Stories/The Greedy Milkman.mp3",
+    title: "The greedy Milkman",
+    category: "STORIES",
+    img: "./images/podcast.jpg",
+  },
+]
+let elementary = [
+  {
+    id: 1,
+    audio: "./podcasts/Stories/Clever Shoemaker.mp3",
+    title: "Clever Shoemaker",
+    category: "ELEMENTARY",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 2,
+    audio: "./podcasts/Stories/Mother Holle.mp3",
+    title: "Mother Holle",
+    category: "ELEMENTARY",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 3,
+    audio: "./podcasts/Stories/The Golden Bird.mp3",
+    title: "The Golden Bird",
+    category: "ELEMENTARY",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 4,
+    audio: "./podcasts/Stories/The Greedy Milkman.mp3",
+    title: "The greedy Milkman",
+    category: "ELEMENTARY",
+    img: "./images/podcast.jpg",
+  },
+]
+let songs = [
+  {
+    id: 17,
+    audio: "./podcasts/Music/EMINEM - TILL I COLLAPSE.mp3",
+    title: "Eminem - Till I Collapse",
+    category: "SONGS",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 18,
+    audio: "./podcasts/Music/JUSTIN BIEBER - LET ME LOVE YOU.m4a",
+    title: "Justin Bieber - Let me love you",
+    category: "SONGS",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 19,
+    audio: "./podcasts/Music/JUSTIN TIMBERLAKE - YOUNG MAN.mp3",
+    title: "Jack Ma vs Elon Musk",
+    category: "SONGS",
+    img: "./images/podcast.jpg",
+  },
+  {
+    id: 20,
+    audio: "./podcasts/Music/WIZ KHALIF ft CHARLIE PUTH - SEE YOU AGAIN.mp3",
+    title: "Wiz Khalif ft Charlie Puth - See you Again",
+    category: "SONGS",
+    img: "./images/podcast.jpg",
+  },
+]
 window.addEventListener("DOMContentLoaded", () => {
   displayAudios(podcasts);
 
@@ -336,12 +568,54 @@ function displayAudios(audios) {
    
     `;
   });
+  
   displayAudio = displayAudio.join("");
   section.innerHTML = displayAudio;
-  let i = section.querySelectorAll("i");
- 
+  let heart = section.querySelectorAll(".heart-btn");
+  heart.forEach((heart)=>{
+
+    heart.addEventListener("click",(e)=>{
+      let target = heart.parentElement.parentElement.parentElement;
+      target.classList.toggle("selected");
+      
+      heart.addEventListener("click",()=>{
+       let icon = target.parentElement.parentElement.parentElement.querySelector("i.far");
+
+     
+         
+        
   
+       
+     })
+    })
+  })
+    let icon = section.querySelectorAll("i.far");
+    icon.forEach((icon)=>{
+      icon.addEventListener("click",()=>{
+        if(icon.classList.contains("far","fa-heart")){
+          icon.classList.remove("far","fa-heart");
+          icon.classList.add("fas","fa-heart");
+        }
+        else{
+          icon.classList.remove("fas","fa-heart");
+          icon.classList.add("far","fa-heart")
+        }
+      })
+    })
 }
+/* function displayPlaylist(audios){
+  
+  
+  let playlist_audio = playlist.querySelector("audio");
+  audio.forEach((audio)=>{
+    if (audio.classList.contains("selected")) {
+      playlist_audio.src = audio.src;
+      console.log(playlist_audio.src);
+      console.log("magic");
+    }
+  })
+
+} */
 function displayBtns() {
   const categories = podcasts.reduce(
     (values, item) => {
@@ -364,6 +638,7 @@ function displayBtns() {
       const category = e.currentTarget.dataset.id;
       const audioCategory = podcasts.filter((audioItem) => {
         if (audioItem.category === category) {
+          
           return audioItem;
         }
       });
@@ -390,8 +665,39 @@ function displayBtns() {
           displayAudios(motivation);
         });
       }
+      if (category === "BUSINESS") {
+        section.innerHTML += `<button class="more">More</button>`;
+        section.querySelector(".more").addEventListener("click", () => {
+          displayAudios(business);
+        });
+      }
+      if (category === "SONGS") {
+        section.innerHTML += `<button class="more">More</button>`;
+        section.querySelector(".more").addEventListener("click", () => {
+          displayAudios(songs);
+        });
+      }
+      if (category === "ELEMENTARY") {
+        section.innerHTML += `<button class="more">More</button>`;
+        section.querySelector(".more").addEventListener("click", () => {
+          displayAudios(elementary);
+        });
+      }
+      if (category === "STORIES") {
+        section.innerHTML += `<button class="more">More</button>`;
+        section.querySelector(".more").addEventListener("click", () => {
+          displayAudios(stories);
+        });
+      }
     });
   });
-}
+}/* 
 let playlist_btn = document.querySelector(".playlist-btn");
-let playlist = document.querySelector(".playlist");
+let playlist = document.querySelector("section.playlist");
+
+playlist_btn.addEventListener("click",()=>{
+  playlist.classList.toggle("open-playlist");
+  
+  displayPlaylist(podcasts)
+})
+ */
